d6a4b5a29143824b7ca3dc4abccfe80ffa9fd81c
hsaljuboori <hsaljubooir.ew@gmail.com>
2022-05-04 05:17:46 +0100

    updated .py filtes

--- test-ospf.py
+++ test-ospf.py
@@ -3,11 +3,8 @@ Author: ALjuboori
 """
 import os
 import sys
-import ipdb
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_command
-from nornir_utils.plugins.functions import print_result
-
 
 config_file = sys.argv[1]
 nr = InitNornir(config_file="config.yaml")
@@ -15,19 +12,17 @@ nr.inventory.defaults.username= os.getenv("USERNAME")
 nr.inventory.defaults.password = os.getenv("PASSWORD")
 
 def pull_info(task):
-  result= task.run(task= send_command, command= "show ip ospf neighbor")
-  task.host["facts"] = result.scrapli_response.genie_parse_output()
-  interfaces = task.host["facts"]["interfaces"]
-  for intf in interfaces:
-    neighbors = interfaces[intf]["neighbors"]
-    for neighbor in neighbors:
-      state= neighbors[neighbor]["state"]
-      return state
+    result= task.run(task= send_command, command= "show ip ospf neighbor")
+    task.host["facts"] = result.scrapli_response.genie_parse_output()
+    interfaces = task.host["facts"]["interfaces"]
+    for intf in interfaces:
+        neighbors = interfaces[intf]["neighbors"]
+        for neighbor in neighbors:
+            state= neighbors[neighbor]["state"]
+            return state
 
 state_result= nr.run(task=pull_info)
 for host in nr.inventory.hosts.values():
   state= state_result[f"{host}"][0].result
   assert "Full" in state, "Failid"
   print("PASSED")
-  
-ipdb.set_trace()
