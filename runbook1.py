4cd1f61ee9a24d9a7b035d4742d0723adf6008a8
Humam Aljuboori <60154854+hsaljuboori@users.noreply.github.com>
2022-05-13 17:13:43 -0400

    Update runbook1.py
    #added this comment to trigger an update#

--- runbook1.py
+++ runbook1.py
@@ -7,6 +7,8 @@ from nornir_utils.plugins.tasks.data import load_yaml
 from nornir_utils.plugins.functions import print_result
 from nornir.core.exceptions import NornirExecutionError
 
+#added this comment to trigger an update#
+
 config_file = sys.argv[1]
 nr = InitNornir(config_file=config_file)
 nr.inventory.defaults.username= os.getenv("USERNAME")
